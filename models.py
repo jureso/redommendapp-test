166db142b95b2663ffe7af70852ce68b20767995
Jure Sokolic <jure.sokolic@gmail.com>
2018-03-18 23:02:31 +0000

    Added missing db models

--- models.py
+++ models.py
@@ -24,6 +24,26 @@ class Place(db.Model):
     return '<place_id: %d, place_name: %s>' % (
     self.place_id, self.place_name)
 
+class Friend(db.Model):
+  __tablename__ = 'friends'
+
+  user_id = db.Column(Integer, primary_key=True)
+  friend_user_id = db.Column(Integer)
+  def __repr__(self):
+    return '<user_id: %d, friend_user_id: %d>' % (
+    self.user_id, self.friend_user_id)
+
+class Rating(db.Model):
+  __tablename__ = 'ratings'
+
+  user_id = db.Column(Integer, primary_key=True)
+  place_id = db.Column(Integer)
+  rating = db.Column(Integer)
+
+  def __repr__(self):
+    return '<user_id: %d, place_id: %d, rating: %d>' % (
+      self.user_id, self.place_id, self.rating)
+
 def example_1():
   from sqlalchemy import create_engine
   db_string = "postgresql://localhost/recommend_app_test"
