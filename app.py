11c2d710fd4ed946c47434e9df55b4a678bab88f
Jure Sokolic <jure.sokolic@gmail.com>
2018-03-18 18:23:08 +0000

    Changed serving model

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-#@app.route('/')
-#def hello():
-#    return "Hello World!"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
