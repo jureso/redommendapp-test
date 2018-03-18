f5dfa1ba35365f33c3c8820df4a7531b8f9746cb
Jure Sokolic <jure.sokolic@gmail.com>
2018-03-18 18:42:07 +0000

    Added flask static serving

--- app.py
+++ app.py
@@ -1,11 +1,71 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+from flask import Flask, render_template, request
+import json
+app = Flask(__name__,static_url_path='')
+
+
+users = {}
+users[0] = {'user_id': 0, 'first_name': 'Jure', 'last_name': 'Sokolic'}
+users[1] = {'user_id': 1, 'first_name': 'Neja', 'last_name': 'Markocic'}
+users[2] = {'user_id': 2, 'first_name': 'Zarja', 'last_name': 'Cibej'}
+users[3] = {'user_id': 3, 'first_name': 'Faris', 'last_name': 'Zacina'}
+
+preferences = {}
+preferences[0] = [{'place_id': 0, 'name': 'McDonalds'}, {'place_id': 4, 'name': 'Wasabi'}]
+preferences[1] = [{'place_id': 4, 'name': 'Wasabi'}]
+preferences[2] = []
+preferences[3] = []
+
+friends = {}
+friends[0] = [{'user_id': 1, 'first_name': 'Neja', 'last_name': 'Markocic'}, {'user_id': 2, 'first_name': 'Zarja', 'last_name': 'Cibej'}]
+friends[1] = []
+friends[2] = [{'user_id': 1, 'first_name': 'Neja', 'last_name': 'Markocic'}]
+friends[3] = []
+
+@app.route("/user_list", methods=["GET"])
+def user_list():
+  data = {}
+  data['user_list'] = list(users.values())
+  response = flask.jsonify(data)
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
+@app.route("/user/<int:user_id>/preferences", methods=["GET"])
+def user_preferences(user_id):
+  data = {}
+  data['preferences'] = preferences[user_id]
+  response = flask.jsonify(data)
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
+@app.route("/user/<int:user_id>/friends", methods=["GET"])
+def user_friends(user_id):
+  data = {}
+  data['friends'] = friends[user_id]
+  response = flask.jsonify(data)
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
+@app.route("/user/<int:user_id>/recommendations", methods=["GET"])
+def user_recommendations(user_id):
+  data = {}
+  data['recommendations'] = preferences[user_id]
+  response = flask.jsonify(data)
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
+
+@app.route("/recommend/<int:user_id>", methods=["GET"])
+def recommend(user_id):
+  recommendations = {}
+  recommendations['recommendations'] = ['McDonalds', 'Wahaca', str(user_id)]
+  return json.dumps(recommendations)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
 
