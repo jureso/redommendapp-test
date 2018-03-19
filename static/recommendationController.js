70818637ff5ee41352adc0c8685171822b43eca2
Jure Sokolic <jure.sokolic@gmail.com>
2018-03-19 21:43:36 +0000

    Added error handling in js and cleanedup the code

--- static/recommendationController.js
+++ static/recommendationController.js
@@ -1,50 +1,45 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('MyCtrl', function($scope, $http) {
-    //$scope.user = "";
-    $scope.selected_user = null;
+var myApp = angular.module('RecommendApp', []);
+myApp.controller('RecommendCtrl', function($scope, $http) {
 
-    $scope.user_preferences = [];
-    $scope.user_friends = [];
-    $scope.recommendations = [];
+        $scope.selected_user = null;
+        $scope.user_preferences = [];
+        $scope.user_friends = [];
+        $scope.recommendations = [];
 
-    $scope.update_data = function () {
-        //$scope.user_preferences = [{name: 'Starbucks'}];
-        //$scope.user_friends = [{user_id: 2, first_name:'Zarja', last_name:'Cibej'}];
-        //$scope.get_user_friends();
-        //$scope.get_user_preferences();
-        $scope.get_user_friends_and_preferences();
-        $scope.get_user_recommendations();
+        $scope.update_data = function () {
+            $scope.get_user_preferences_and_friends();
+            $scope.get_user_recommendations();
         };
 
+        $scope.get_user_list = function () {
+            $http.get('/user_list').then(
+                function successCallback(response) {
+                    $scope.user_list = response.data.user_list;
+                },
+                function errorCallback(response) {
+                    console.log(response.status, response.statusText);
+                });
+        };
+        $scope.get_user_preferences_and_friends = function () {
+            $http.get('/user/' + $scope.selected_user.user_id.toString() + '/preferences_and_friends').then(
+                function successCallback(response) {
+                    $scope.user_friends = response.data.friends_list;
+                    $scope.user_preferences = response.data.ratings_list;
+                },
+                function errorCallback(response) {
+                    console.log(response.status, response.statusText);
+                });
+        };
+        $scope.get_user_recommendations = function () {
+            $http.get('/user/' + $scope.selected_user.user_id.toString() + '/recommendations').then(
+                function successCallback(response) {
+                    $scope.recommendations = response.data.recommendations;
+                },
+                function errorCallback(response) {
+                    console.log(response.status, response.statusText);
+                });
+        };
 
-    $scope.get_user_list = function () {
-        $http.get('/user_list').then(function(response) {
-            $scope.user_list = response.data.user_list;
-        });
-
-    };
-
-    $scope.get_user_friends_and_preferences = function () {
-        $http.get('/user/'+ $scope.selected_user.user_id.toString() +'/preferences_and_friends').then(function(response) {
-            $scope.user_friends = response.data.friends_list;
-            $scope.user_preferences = response.data.ratings_list;
-        });
-    };
-
-    $scope.get_user_recommendations = function () {
-        $http.get('/user/'+ $scope.selected_user.user_id.toString() +'/recommendations').then(function(response) {
-            $scope.recommendations = response.data.recommendations;
-        });
-
-    };
-
-    //$scope.user_list=[{user_id: 0, first_name:'Jure', last_name:'Sokolic'},
-    //             {user_id: 1, first_name:'Neja', last_name:'Markocic'},
-    //             {user_id: 2, first_name:'Zarja', last_name:'Cibej'}];
-    $scope.get_user_list();
-
+        $scope.get_user_list();
     }
-
-
 );
-//TODO: How do we adaptively set request ip?
